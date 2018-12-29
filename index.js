7ed258066a11cb5a13a4ed93dff241f54a717b83
lydstyl <lydstyl@gmail.com>
2018-12-29 20:01:11 +0100

    loulous

--- index.js
+++ index.js
@@ -2,28 +2,35 @@ if ( typeof require !== 'undefined' ) XLSX = require('xlsx')
 const workbook = XLSX.readFile( 'OrderHistory.xlsx' )
 
 // console.log(workbook.Sheets.sheet1.A1.v)
+const lines = []
+let line = {}
 Object.keys( workbook.Sheets.sheet1 ).forEach( cellName => {
-    console.log( `\nCELL ${cellName}` )
+    //console.log( `\nCELL ${cellName}` )
     const cell = workbook.Sheets.sheet1[cellName]
-    console.log(cell.v)
-
     const letter =  cellName.charAt(0)
     
     console.log(letter);
-
-    switch (letter) {
-        case 'A':
-            
-            break;
-    
-        default:
-            break;
+    // Date(UTC)	Pair	Type	Order Price	Order Amount	Avg Trading Price	Filled	Total	status
+    if (letter === 'A') {
+        lines.push(line)
+        line = []
     }
+    line.push(cell.v)
+})
+console.log(lines[0][0]);
+let first = true
+lines.forEach(line => {
+    for (let i = 0; i < line.length; i++) {
+        const cell = line[i];
+        if (first) {
+            
+         //   console.log('ee ' +line);
+        }
+    }
+    first = false
+});
+
 
     // si Adate et 
         // IFilled
     // pair: {type, avgPrice, qte 
-
-
-
-})
