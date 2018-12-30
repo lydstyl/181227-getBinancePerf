8c5af5f8b17451746b4b91de7340c89b5589d7c3
lydstyl <lydstyl@gmail.com>
2018-12-30 15:25:40 +0100

    object to work ok

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
+const opts = {
+    lastCell: 'I152'
+}
 if ( typeof require !== 'undefined' ) XLSX = require('xlsx')
 const workbook = XLSX.readFile( 'OrderHistory.xlsx' )
-
-// console.log(workbook.Sheets.sheet1.A1.v)
 const lines = []
-let line = {}
+let line = []
+let newLine = false
 Object.keys( workbook.Sheets.sheet1 ).forEach( cellName => {
-    //console.log( `\nCELL ${cellName}` )
     const cell = workbook.Sheets.sheet1[cellName]
     const letter =  cellName.charAt(0)
-    
-    console.log(letter);
-    // Date(UTC)	Pair	Type	Order Price	Order Amount	Avg Trading Price	Filled	Total	status
-    if (letter === 'A') {
-        lines.push(line)
-        line = []
+    if (letter === 'A' && cellName != 'A1') {
+        newLine = true
+        line = [] //  new line
     }
-    line.push(cell.v)
+    if (newLine) line.push(cell.v)
+    if (letter === 'I' && cell.v == 'Filled') {
+        lines.push(line)
+        newLine = false
+    }
 })
-console.log(lines[0][0]);
-let first = true
+// console.log(lines.length);
+
+// Date(UTC)	Pair	Type	Order Price	Order Amount	Avg Trading Price	Filled	Total	status
+const pairs = {}
 lines.forEach(line => {
-    for (let i = 0; i < line.length; i++) {
-        const cell = line[i];
-        if (first) {
-            
-         //   console.log('ee ' +line);
+    if ( !pairs[line[1]] ) { // eg paris.ETHBTC
+        pairs[line[1]] = {
+            BUY: [],
+            SELL: []
         }
     }
-    first = false
+
+    let date = line[0]
+    let type = line[2]
+    let price = !line[5] ? line[3] : line[5]
+    let total = line[7]
+    
+    pairs[line[1]][type].push( {date: date, price: price, total: total} )
 });
 
+console.log(pairs);
 
     // si Adate et 
         // IFilled
