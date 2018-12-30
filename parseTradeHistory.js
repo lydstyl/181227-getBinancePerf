210a1d536cd969ec7e5ecde144e5739c56674557
lydstyl <lydstyl@gmail.com>
2018-12-30 20:32:06 +0100

    obj for csv ok

--- parseTradeHistory.js
+++ parseTradeHistory.js
@@ -34,6 +34,52 @@ transactions.forEach(transaction => {
             total: parseFloat(transaction[5]),
         })
     }
-});
+})
+csvObj = {}
+Object.keys(trades).forEach(pair => {
+    let transactions = trades[pair]
+    csvObj[pair] = {}
+    transactions.forEach(t => {
+        if ( !csvObj[pair].START || t.date < csvObj[pair].START ) {
+            csvObj[pair].START = t.date
+        }
+        if ( !csvObj[pair].FINISH || t.date > csvObj[pair].FINISH ) {
+            csvObj[pair].FINISH = t.date
+        }
+        if ( t.type === 'BUY' ) {
+            if ( !csvObj[pair].buyAmount ) {
+                csvObj[pair].buyAmount = 0
+            }
+            csvObj[pair].buyAmount += t.amount
+            if ( !csvObj[pair].totalBuy ) {
+                csvObj[pair].totalBuy = 0
+            }
+            csvObj[pair].totalBuy += t.total
+        }
+        if ( t.type === 'SELL' ) {
+            if ( !csvObj[pair].sellAmount ) {
+                csvObj[pair].sellAmount = 0
+            }
+            csvObj[pair].sellAmount += t.amount
+            if ( !csvObj[pair].totalSell ) {
+                csvObj[pair].totalSell = 0
+            }
+            csvObj[pair].totalSell += t.total
+        }
+    })
+    csvObj[pair].buyMinusSellAmount = csvObj[pair].buyAmount - csvObj[pair].sellAmount
+    if
+    ( 
+        (csvObj[pair].buyAmount / csvObj[pair].sellAmount) <= 1.01 &&
+        (csvObj[pair].sellAmount / csvObj[pair].buyAmount) <= 1.01 
+    ) {
+        csvObj[pair].AVGBUY = csvObj[pair].totalBuy / csvObj[pair].buyAmount
+        csvObj[pair].AVGSELL = csvObj[pair].totalSell / csvObj[pair].sellAmount
+        csvObj[pair].PERFNOFEE = (100 * csvObj[pair].AVGSELL / csvObj[pair].AVGBUY) - 100
+        csvObj[pair].GAINWITHFEES = (csvObj[pair].totalBuy - csvObj[pair].totalSell) * 0.998
+    }else{
+        delete csvObj[pair]
+    }
+})
+console.log(csvObj)
 // START	FINISH	PAIR	AVG-BUY	AVG-SELL	PERF-NO-FEE	GAIN-WITH-FEES
-console.log(trades);
